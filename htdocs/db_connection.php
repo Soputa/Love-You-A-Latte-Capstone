afff9d825e37732c997efe29bdf71bca92c6dbcf
Giovanni Glorio <90005783+giomonk@users.noreply.github.com>
2021-09-28 18:27:44 -0400

    Update db_connection.php
    this is my fix, maybe

--- htdocs/db_connection.php
+++ htdocs/db_connection.php
@@ -1,16 +1,19 @@
 <?php
-$host ="yellowdb.cga6xtmk1gfd.us-east-2.rds.amazonaws.com"; 
-$username ="yellow";
-$password ="YellowTeam-02";
-$database ="loveyoualatte";
-
-mssql_connect($host, $username, $password);
-mssql_select_db($database);
-
-if( $conn ) {
-    echo "Connection established.<br />";
-}else{
-    echo "Connection could not be established.<br />";
-    die( print_r( sqlsrv_errors(), true));
-}
+// This is Gio's code
+function OpenCon()
+ {
+ $dbhost = "yellowdb.cga6xtmk1gfd.us-east-2.rds.amazonaws.com";
+ $dbuser = "yellow";
+ $dbpass = "YellowTeam-02";
+ $db = "loveyoualatte";
+ $conn = new mssql($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
 ?>
